46fa27dd6b309c452dff8a61fe538ed6573de713
coloradc <coloradc@mskcc.org>
2023-05-12 17:20:00 -0400

    Implemented getCustomers

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,48 +9,104 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// define a global variable for the content type header
+var contentType string = "Content-Type"
+var contentTypeJSON string = "application/json"
+
 type Customer struct {
 	ID        string `json:"id"`
 	Name      string `json:"name"`
 	Role      string `json:"role"`
 	Email     string `json:"email"`
-	Phone     uint16 `json:"phone"`
+	Phone     int    `json:"phone"`
 	Contacted bool   `json:"contacted"`
 }
 
-var customers []Customer
+type errorResponse struct {
+	StatusCode int    `json:"code"`
+	Message    string `json:"message"`
+}
+
+var customers = []Customer{}
 
 func getCustomers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set(contentType, contentTypeJSON)
 	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(customers)
 }
 
 func getCustomer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	w.Header().Set(contentType, contentTypeJSON)
+	id := mux.Vars(r)["id"]
+	for _, customer := range customers {
+		if customer.ID == id {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(customer)
+			return
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			error := errorResponse{
+				StatusCode: http.StatusNotFound,
+				Message:    "Customer not found",
+			}
+			json.NewEncoder(w).Encode(error)
+			return
+		}
+	}
 }
 
 func addCustomer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set(contentType, contentTypeJSON)
 	w.WriteHeader(http.StatusCreated)
 }
 
 func updateCustomer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set(contentType, contentTypeJSON)
 	w.WriteHeader(http.StatusOK)
 }
 func deleteCustomer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set(contentType, contentTypeJSON)
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
-	// Instantiate a new router by invoking the "NewRouter" handler
+	// Initialize mock data
+	customers = append(customers,
+		Customer{
+			ID:        "1",
+			Name:      "John Doe",
+			Role:      "CEO",
+			Email:     "joDoe@email.com",
+			Phone:     5550199,
+			Contacted: true,
+		},
+		Customer{
+			ID:        "2",
+			Name:      "Jane Doe",
+			Role:      "CTO",
+			Email:     "jaDoe@email.com",
+			Phone:     5550199,
+			Contacted: false,
+		},
+		Customer{
+			ID:        "3",
+			Name:      "Cesar Colorado",
+			Role:      "Software Engineer",
+			Email:     "cesar@mail.com",
+			Phone:     555555,
+			Contacted: true,
+		})
+	// Init a new router by invoking the "NewRouter" handler
 	router := mux.NewRouter()
+
+	// Define the URl constant instead of duplicating the string
+	customerUri := "/customers"
 	customerIdUri := "/customers/{id}"
-	router.HandleFunc("/customers", getCustomers).Methods("GET")
-	router.HandleFunc(customerIdUri, getCustomers).Methods("GET")
-	router.HandleFunc("/customers", addCustomer).Methods("POST")
+
+	// Define the routes
+	router.HandleFunc(customerUri, getCustomers).Methods("GET")
+	router.HandleFunc(customerUri, addCustomer).Methods("POST")
+	router.HandleFunc(customerIdUri, getCustomer).Methods("GET")
 	router.HandleFunc(customerIdUri, deleteCustomer).Methods("DELETE")
 	router.HandleFunc(customerIdUri, updateCustomer).Methods("PUT")
 
